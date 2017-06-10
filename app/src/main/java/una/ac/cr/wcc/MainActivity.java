f85a2a4f6b41e67a2e568d5ddd27e62c151c46e9
bfhg17 <bfhg.17@hotmail.com>
2017-06-09 23:41:43 -0600

    Cambios a la GUI,  nuevos drawable, elimine los hardcoded que habían de strings, ahora están declarados en strins.xml, Incorporación de la vista de registro

--- app/src/main/java/una/ac/cr/wcc/MainActivity.java
+++ app/src/main/java/una/ac/cr/wcc/MainActivity.java
@@ -8,20 +8,29 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnIngresar;
-
+    Button btnIngresar,btnRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnIngresar = (Button)findViewById(R.id.btnIngresar);
-
+        btnRegistrar = (Button)findViewById(R.id.Btn_Registrar);
         btnIngresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Registro.class);
                 startActivity(intent);
 
             }
