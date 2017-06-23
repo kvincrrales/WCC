e1d8f946e5679f0c8363843b8af8b05fedab4763
Bryan <bryanperezz1992@gmail.com>
2017-06-23 10:48:44 -0600

    validacion de registro en registro de usuario, metodos en el activity

--- app/src/main/java/una/ac/cr/wcc/RegistrarUsuariosActivity.java
+++ app/src/main/java/una/ac/cr/wcc/RegistrarUsuariosActivity.java
@@ -5,12 +5,18 @@ import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class RegistrarUsuariosActivity extends AppCompatActivity {
+    private EditText et_nombreUsuario_input, et_emailUsuario_input, et_passUsuario_input;
+    private String nombreUsuario_input, emailUsuario_input, passUsuario_input;
+    Button btn_update;
+
     EditText nombreUsuario,emailUsuario,passUsuario;
     TextView textView;
     BD_Controlador controller;
@@ -27,6 +33,50 @@ public class RegistrarUsuariosActivity extends AppCompatActivity {
         textView = (TextView)findViewById(R.id.textView);
 
         controller = new BD_Controlador(this,"",null,1);
+
+        et_nombreUsuario_input = (EditText) findViewById(R.id.nombreUsuario_input);
+        et_emailUsuario_input = (EditText) findViewById(R.id.emailUsuario_input);
+        et_passUsuario_input = (EditText) findViewById(R.id.passUsuario_input);
+        btn_update = (Button) findViewById(R.id.btn_update);
+        btn_update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registroUsuario();
+            }
+        });
+    }
+
+    public void registroUsuario() {
+        intiliaze();
+        if (!validate()) {
+            Toast.makeText(this, "Fallo el registro", Toast.LENGTH_SHORT).show();
+        } else {
+            onSignupSuccess();
+        }
+    }
+    public void onSignupSuccess(){
+
+    }
+    public boolean validate(){
+        boolean valid = true;
+        if(nombreUsuario_input.isEmpty()||nombreUsuario_input.length()>15){
+            et_nombreUsuario_input.setError("Ingrese un nombre de usuario");
+            valid = false;
+        }
+        if(emailUsuario_input.isEmpty()|| Patterns.EMAIL_ADDRESS.matcher(emailUsuario_input).matches()){
+            et_emailUsuario_input.setError("Ingrese un email");
+            valid = false;
+        }
+        if(passUsuario_input.isEmpty()){
+            et_passUsuario_input.setError("Ingrese un password");
+            valid = false;
+        }
+        return valid;
+    }
+    public void intiliaze(){
+        nombreUsuario_input = et_nombreUsuario_input.getText().toString().trim();
+        emailUsuario_input = et_emailUsuario_input.getText().toString().trim();
+        passUsuario_input = et_passUsuario_input.getText().toString().trim();
     }
 
     public void btn_click(View view){
